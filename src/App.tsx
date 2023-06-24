111e6e36865de1db5edaeb77379ed5f7bcf85d86
dnikitha123 <136118487+dnikitha123@users.noreply.github.com>
2023-06-24 18:46:13 +0530

    React Ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import JobList from "./components/Job/JobList";
+import JobProvider from "./components/Job/JobProvider";
 
-function App() {
+
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <JobProvider>
+        <JobList/>
+      </JobProvider>
     </div>
   );
-}
+};
 
 export default App;
